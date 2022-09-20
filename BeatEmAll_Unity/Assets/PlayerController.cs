7f00f7f6f1a2ec6e0710ea5d97ed16301385d50d
GSaillenfest <104986291+GSaillenfest@users.noreply.github.com>
2022-09-20 12:13:59 +0200

    Player movements, Sprite sheets colliders

--- BeatEmAll_Unity/Assets/PlayerController.cs
+++ BeatEmAll_Unity/Assets/PlayerController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Rigidbody2D playerRb;
+    [SerializeField] float speed = 5f;
+    [SerializeField] Animator animator;
+
+    Vector2 direction;
+    float horizontalInput;
+    float verticalInput;
+    bool isWalking = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        direction = new Vector2 (horizontalInput, verticalInput).normalized;
+        if (direction.magnitude > 0) isWalking = true;
+        else isWalking = false;
+
+        animator.SetBool("isWalking", isWalking);
+
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.velocity = speed * direction;
     }
 }
