98aec0fd48cb44513aaa23720a221d33eec9b3e0
Imane BOUROUYNE (MA/FR) <110111059+Curiosity10001@users.noreply.github.com>
2022-09-22 17:16:12 +0200

    Animator +script for player with can

--- BeatEmAll_Unity/Assets/Scripts/PlayerHasCan.cs
+++ BeatEmAll_Unity/Assets/Scripts/PlayerHasCan.cs
@@ -2,26 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHasCan: MonoBehaviour
+public class PlayerHasCan : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerRb;
     [SerializeField] Animator animator;
-   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        WithCan();
+    }
     public void WithCan()
     {
-        void OncollisionEnterCan(Collision2D collision)
+
+        if (animator.GetBool("isWalking") == true && animator.GetBool("HasCan") == true)
         {
-            if (collision.gameObject.CompareTag("can")
-            { 
-                animator.SetBool(HasCan,true)
+            animator.SetBool("WalkWCan", true);
+        }
+
+        if (animator.GetBool("isJumping") == true && animator.GetBool("HasCan") == true)
+        {
+            animator.SetTrigger("JumpWCan");
+        }
+
+        if (animator.GetBool("HasCan") == true && animator.GetBool("isWalking") == false && animator.GetBool("isJumping") == false)
+        {
+            animator.SetBool("WalkWCan", true);
+        }
+
+        if (animator.GetBool("HasCan") == true && Input.GetButtonDown("fire1"))
+        {
+            animator.SetTrigger("LaunchCan");
+            animator.SetBool("HasCan", false);
+            CanLaunch();
+        }
+        
+
+
+
+
+
+        }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("can") && Input.GetButtonDown("fire1"))
+        {
+            animator.SetBool("HasCan", true);
+            animator.SetTrigger("PickUpCan");
+            CanLift();
+            if (collision.gameObject.CompareTag("Enemy") && animator.GetBool("HasCan") == true)
+            {
+                animator.SetBool("HasCan", false);
+                animator.SetBool("LoseCan", true);
             }
         }
+
     }
-}
+    public void CanLaunch() { }//to do
+    public void CanLift(){}//to do
+ }
